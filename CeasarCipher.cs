8158dc1f9927b2fbd3b0a58c29220911cf824094
Batu YURT <asdasd018@hotmail.com>
2024-05-20 17:33:33 +0300

    CeasarCipher

---
+++ CeasarCipher.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions
+{
+    public class CeasarCipher
+    {
+        public CeasarCipher()
+        {
+            var cipheredMsg = cipherMsg(3, "attack");
+            decipherMsg(3, cipheredMsg);
+        }
+
+        StringBuilder resultBuilder = new StringBuilder();
+        char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+                'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+                't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+        private string cipherMsg(int rotaNumber, string msg)
+        {
+            char[] msgCharArray = msg.ToCharArray();
+            resultBuilder.Clear();
+            foreach (char letter in msgCharArray)
+            {
+                int letterIndex = Array.IndexOf(alphabet, letter);
+                resultBuilder.Append(alphabet[letterIndex + rotaNumber]);
+            }
+            Console.WriteLine(resultBuilder.ToString());
+            return resultBuilder.ToString();
+        }
+
+        private string decipherMsg(int rotaNumber, string msg)
+        {
+            char[] msgCharArray = msg.ToCharArray();
+            resultBuilder.Clear();
+            foreach (char letter in msgCharArray)
+            {
+                int letterIndex = Array.IndexOf(alphabet, letter);
+                resultBuilder.Append(alphabet[letterIndex - rotaNumber]);
+            }
+            Console.WriteLine(resultBuilder.ToString());
+            return resultBuilder.ToString();
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,3 +6,4 @@ Console.WriteLine("Hello, World!");
 
 //new AppendAndDelete();
 //new PickingNumbers();
+//new CeasarCipher();
