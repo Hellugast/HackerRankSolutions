5ae7d317ee14cc15b88a7d6b2902ab1d481083bd
Batu YURT <asdasd018@hotmail.com>
2024-05-23 21:31:50 +0300

    FindDigits

---
+++ FindDigits.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions
+{
+    public class FindDigits
+    {
+        public FindDigits()
+        {
+            findDigits(1012);
+            findDigits(1012112);
+        }
+
+        int findDigits(int n)
+        {
+            int result = 0;
+            int divider = 10;
+
+            while (true)
+            {
+                if ((n * 10 / divider) > 0)
+                {
+                    if ((n * 10 / divider) % 10 == 0)
+                    {
+                        divider *= 10;
+                        continue;
+                    }
+                    if (n % ((n * 10 / divider) % 10) == 0) result++;
+
+                    divider *= 10;
+                }
+                else
+                    break;
+            }
+            return result;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,3 +7,4 @@ Console.WriteLine("Hello, World!");
 //new AppendAndDelete();
 //new PickingNumbers();
 //new CeasarCipher();
+//new FindDigits();
